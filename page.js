396003a4e23fa3028d7124d05b20f806b9936d51
Zach Perlman <zach.perlman@wustl.edu>
2018-06-21 17:26:38 -0400

    button

--- page.js
+++ page.js
@@ -1,9 +1,12 @@
 function sunsetbutton(){
   window.location.href = 'https://www.buzzfeed.com/erinchack/places-nyc-locals-go-to-watch-the-sunset?utm_term=.huolWGGy#.kl20B443';
 }
-
+function roll(){
+  window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
 //listeners
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('sunsetbutton').addEventListener("click", sunsetbutton) //go to advanced-features
+  document.getElementById('sunsetbutton').addEventListener("click", sunsetbutton)
+  document.getElementById('second').addEventListener("click", roll)
   console.log("started"); //post on console that the JavaScript has started working
 });
