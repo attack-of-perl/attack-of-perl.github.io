a6f4b7212c89a93563c3f66ebb329290e6fe1a31
Zach Perlman <zach.perlman@wustl.edu>
2018-06-21 17:52:14 -0400

    easy

--- page.js
+++ page.js
@@ -2,9 +2,6 @@
 
 //listeners
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('second').addEventListener("click", roll)
-  document.getElementById('sunsetbutton').addEventListener("click", sunsetbutton)
 
-  document.getElementById('insta').addEventListener("click", idibari)
   console.log("started"); //post on console that the JavaScript has started working
 });
