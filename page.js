cb108f0ecfd656ba9f8e7ee1184e4e6bd58539f2
Zach Perlman <zach.perlman@wustl.edu>
2018-06-21 17:04:17 -0400

    button

--- page.js
+++ page.js
@@ -1,1 +1,9 @@
-//nothing here yet
+function sunsetbutton(){
+  window.location.href = 'http://www.google.com';
+}
+
+//listeners
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById('sunsetbutton').addEventListener("click", sunsetbutton) //go to advanced-features
+  console.log("started"); //post on console that the JavaScript has started working
+});
