3c1e967e9f38b82dcfbeac8691103ba71c3f5af7
Zach Perlman <zach.perlman@wustl.edu>
2018-06-21 17:05:08 -0400

    page

--- page.js
+++ page.js
@@ -1,5 +1,5 @@
 function sunsetbutton(){
-  window.location.href = 'http://www.google.com';
+  window.location.href = 'https://www.google.com/search?q=best+places+to+see+the+sunset+in+nyc&rlz=1C5CHFA_enUS764US764&oq=best+places+to+see+the+sunset&aqs=chrome.1.69i57j0l5.6605j1j7&sourceid=chrome&ie=UTF-8';
 }
 
 //listeners
