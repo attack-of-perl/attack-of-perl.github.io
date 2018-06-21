9e7cbd443d7941d413b1303b58facc17506430f3
Zach Perlman <zach.perlman@wustl.edu>
2018-06-21 17:30:00 -0400

    idi

--- page.js
+++ page.js
@@ -4,9 +4,14 @@ function sunsetbutton(){
 function roll(){
   window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 }
+function idibari(){
+  window.location.href = 'https://www.instagram.com/idibari/?hl=en';
+}
+
 //listeners
 document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById('sunsetbutton').addEventListener("click", sunsetbutton)
   document.getElementById('second').addEventListener("click", roll)
+  document.getElementById('idibari').addEventListener("click", idibari)
   console.log("started"); //post on console that the JavaScript has started working
 });
